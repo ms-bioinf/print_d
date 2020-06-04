5b59f7335e09d7785d9e97ee4f9e9c2ec5ce4c4c
ms-bioinf <66415141+ms-bioinf@users.noreply.github.com>
2020-06-04 14:46:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Code to print matrices for the applied bioinformatics lecture.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ms-bioinf/print_d",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
